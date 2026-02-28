1eebd929e5a0988bdab31b65c68e007193c8701f
caotianhao <caotianhao@naver.com>
2026-02-28 17:03:09 +0800

    第5层画完了，差个机关门事件

--- project/floors/MT5.js
+++ project/floors/MT5.js
@@ -1,42 +1,57 @@
 main.floors.MT5=
 {
-"floorId": "MT5",
-"title": "主塔 5 层",
-"name": "5",
-"width": 13,
-"height": 13,
-"canFlyTo": true,
-"canFlyFrom": true,
-"canUseQuickShop": true,
-"images": [],
-"ratio": 1,
-"defaultGround": "ground",
-"bgm": "bgm.mp3",
-"firstArrive": [],
-"eachArrive": [],
-"parallelDo": "",
-"events": {},
-"changeFloor": {},
-"beforeBattle": {},
-"afterBattle": {},
-"afterGetItem": {},
-"afterOpenDoor": {},
-"autoEvent": {},
-"cannotMove": {},
-"cannotMoveIn": {},
-"map": [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    "floorId": "MT5",
+    "title": "主塔 5 层",
+    "name": "5",
+    "width": 13,
+    "height": 13,
+    "canFlyTo": true,
+    "canFlyFrom": true,
+    "canUseQuickShop": true,
+    "images": [],
+    "ratio": 1,
+    "defaultGround": "ground",
+    "bgm": "bgm.mp3",
+    "firstArrive": [],
+    "eachArrive": [],
+    "parallelDo": "",
+    "events": {},
+    "changeFloor": {
+        "1,11": {
+            "floorId": ":next",
+            "stair": "downFloor"
+        },
+        "11,11": {
+            "floorId": ":before",
+            "stair": "upFloor"
+        }
+    },
+    "beforeBattle": {},
+    "afterBattle": {},
+    "afterGetItem": {},
+    "afterOpenDoor": {},
+    "autoEvent": {},
+    "cannotMove": {},
+    "cannotMoveIn": {},
+    "map": [
+    [  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
+    [  1, 34, 34,  1,  1,  0, 23,  0,  1,  0, 34,  0,  1],
+    [  1, 30, 30, 82, 82, 34,  0, 34,  1, 28,  0, 27,  1],
+    [  1,  1,211,  1,  1,  1, 85,  1,  1,  1, 82,  1,  1],
+    [  1, 29, 29, 29,  1,221,  0,221,  1, 21,211, 29,  1],
+    [  1,  1,217,  1,  1,  1, 81,  1,  1,207,  1,  1,  1],
+    [  1, 21,  0,217,  1,206,  0,206,210,  0, 81, 31,  1],
+    [  1,207,  1,  0, 81,  0,  1,  0,  1,203,  1, 32,  1],
+    [  1,  0, 81, 32,  1,206,  0,206, 81,  0, 21,  1,  1],
+    [  1,212,  1,210,  1,  1,207,  1,  1,210,  1, 32,  1],
+    [  1,  0, 82,  0,  0,211,  0,  0, 82,  0,210,  0,  1],
+    [  1, 87,  1, 22, 21,  1, 29, 27,  1, 31,  1, 88,  1],
+    [  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1]
 ],
+    "bgmap": [
+
+],
+    "fgmap": [
+
+]
 }
