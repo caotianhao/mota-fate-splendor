483a65648dee5b1074256830606d4c82e0165098
caotianhao <caotianhao@naver.com>
2026-03-20 22:15:08 +0800

    -10f 剧情1

--- project/data.js
+++ project/data.js
@@ -75,7 +75,11 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 			"MT50",
 			"EX10",
 			"EX20",
-			"EX30"
+			"EX30",
+			"EXA101",
+			"EXA102",
+			"EXA103",
+			"EXA104"
 		],
 		"floorPartitions": [],
 		"images": [
---
+++ project/floors/EXA101.js
@@ -0,0 +1,49 @@
+main.floors.EXA101=
+{
+    "floorId": "EXA101",
+    "title": "",
+    "name": "",
+    "width": 13,
+    "height": 13,
+    "canFlyTo": false,
+    "canFlyFrom": false,
+    "canUseQuickShop": false,
+    "cannotViewMap": true,
+    "cannotMoveDirectly": true,
+    "images": [],
+    "ratio": 1,
+    "defaultGround": "ground",
+    "firstArrive": [],
+    "eachArrive": [],
+    "parallelDo": "",
+    "events": {},
+    "changeFloor": {},
+    "beforeBattle": {},
+    "afterBattle": {},
+    "afterGetItem": {},
+    "afterOpenDoor": {},
+    "autoEvent": {},
+    "cannotMove": {},
+    "cannotMoveIn": {},
+    "map": [
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
+],
+    "bgmap": [
+
+],
+    "fgmap": [
+
+]
+}
