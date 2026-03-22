6629e9750374a34d7188a051462891d1c09cc780
caotianhao <caotianhao@naver.com>
2026-03-22 19:56:44 +0800

    结局事件5

--- project/floors/MT50.js
+++ project/floors/MT50.js
@@ -12,10 +12,272 @@ main.floors.MT50=
     "ratio": 3,
     "defaultGround": "ground",
     "bgm": "bgm.mp3",
-    "firstArrive": [],
+    "firstArrive": [
+        {
+            "type": "sleep",
+            "time": 500,
+            "noSkip": true
+        },
+        "\t[勇士,hero]\b[up,null]这里就是意识空间的尽头了。",
+        "\t[勇士,hero]\b[up,null]前方等待着我与她的，将会是怎样的命运？"
+    ],
     "eachArrive": [],
     "parallelDo": "",
-    "events": {},
+    "events": {
+        "6,9": [
+            {
+                "type": "vibrate",
+                "direction": "random",
+                "time": 800,
+                "speed": 8,
+                "power": 8
+            },
+            {
+                "type": "moveHero",
+                "time": 1200,
+                "steps": [
+                    "up:1"
+                ]
+            },
+            "\t[勇者虚影,N1632]\b[up,null]不错，很能干嘛，在经历了无数遍的轮回后，你终于首次突破到这里来了，名为zeno的勇士灵魂。",
+            "\t[勇者虚影,N1632]\b[up,null]你还记得我是谁吗？",
+            "\t[勇士,hero]\b[up,null]曾经击败我的那个勇者，你的脸，我这辈子都不会忘记。",
+            "\t[勇者虚影,N1632]\b[up,null]看样子，在吸收完所有曾经被你杀死的魔物灵魂后，你已经恢复成之前的全部实力了。",
+            "\t[勇士,hero]\b[up,null]是的，我恢复的东西里包括你拆散我跟她的记忆，以及你为了一己私情而毁灭魔塔的记忆，我全都回想起来了。",
+            {
+                "type": "sleep",
+                "time": 500,
+                "noSkip": true
+            },
+            {
+                "type": "screenFlash",
+                "color": [
+                    255,
+                    255,
+                    255,
+                    1
+                ],
+                "time": 500,
+                "times": 1
+            },
+            {
+                "type": "jumpHero",
+                "dxy": [
+                    0,
+                    -1
+                ],
+                "time": 1
+            },
+            {
+                "type": "animate",
+                "name": "hand",
+                "loc": [
+                    6,
+                    6
+                ],
+                "async": true
+            },
+            {
+                "type": "animate",
+                "name": "hand",
+                "loc": [
+                    6,
+                    7
+                ],
+                "async": true
+            },
+            {
+                "type": "waitAsync"
+            },
+            {
+                "type": "animate",
+                "name": "sword",
+                "loc": [
+                    6,
+                    6
+                ],
+                "async": true
+            },
+            {
+                "type": "animate",
+                "name": "sword",
+                "loc": [
+                    6,
+                    7
+                ],
+                "async": true
+            },
+            {
+                "type": "waitAsync"
+            },
+            {
+                "type": "animate",
+                "name": "jianji",
+                "loc": [
+                    6,
+                    6
+                ],
+                "async": true
+            },
+            {
+                "type": "animate",
+                "name": "jianji",
+                "loc": [
+                    6,
+                    7
+                ],
+                "async": true
+            },
+            {
+                "type": "waitAsync"
+            },
+            {
+                "type": "animate",
+                "name": "jianji",
+                "loc": [
+                    6,
+                    6
+                ],
+                "async": true
+            },
+            {
+                "type": "animate",
+                "name": "jianji",
+                "loc": [
+                    6,
+                    7
+                ],
+                "async": true
+            },
+            {
+                "type": "waitAsync"
+            },
+            {
+                "type": "jump",
+                "from": [
+                    6,
+                    6
+                ],
+                "to": [
+                    8,
+                    5
+                ],
+                "time": 200,
+                "keep": true,
+                "async": true
+            },
+            {
+                "type": "jumpHero",
+                "dxy": [
+                    2,
+                    -1
+                ],
+                "time": 200,
+                "async": true
+            },
+            {
+                "type": "waitAsync"
+            },
+            {
+                "type": "animate",
+                "name": "jianji",
+                "loc": [
+                    8,
+                    6
+                ],
+                "async": true
+            },
+            {
+                "type": "animate",
+                "name": "jianji",
+                "loc": [
+                    8,
+                    5
+                ],
+                "async": true
+            },
+            {
+                "type": "waitAsync"
+            },
+            {
+                "type": "animate",
+                "name": "sword",
+                "loc": [
+                    8,
+                    6
+                ],
+                "async": true
+            },
+            {
+                "type": "animate",
+                "name": "sword",
+                "loc": [
+                    8,
+                    5
+                ],
+                "async": true
+            },
+            {
+                "type": "waitAsync"
+            },
+            {
+                "type": "screenFlash",
+                "color": [
+                    255,
+                    255,
+                    255,
+                    1
+                ],
+                "time": 500,
+                "times": 1,
+                "async": true
+            },
+            {
+                "type": "hide",
+                "loc": [
+                    [
+                        8,
+                        5
+                    ]
+                ],
+                "remove": true,
+                "time": 1,
+                "async": true
+            },
+            {
+                "type": "setBlock",
+                "number": "N1577",
+                "loc": [
+                    [
+                        4,
+                        8
+                    ]
+                ],
+                "time": 1,
+                "async": true
+            },
+            {
+                "type": "jumpHero",
+                "dxy": [
+                    -3,
+                    2
+                ],
+                "time": 1,
+                "async": true
+            },
+            {
+                "type": "changePos",
+                "loc": [
+                    5,
+                    8
+                ],
+                "direction": "left"
+            },
+            {
+                "type": "waitAsync"
+            }
+        ]
+    },
     "changeFloor": {},
     "beforeBattle": {},
     "afterBattle": {},
